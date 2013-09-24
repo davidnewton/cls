2127e3093719ddf0ab89b257db21769d8a2b622a
davidnewton <david.newton@esendex.com>
2013-09-24 21:01:01 +0100

    added nodes config for logstash

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,6 @@
 node 'log-01' {
   include puppet
+  include logstash
 }
 node 'log-02' {
   include puppet
---
+++ modules/logstash/manifests/init.pp
@@ -0,0 +1,30 @@
+class logstash {
+  file {"/opt/logstash-1.1.0-monolithic.jar":
+    source => "puppet:///modules/logstash/logstash-1.2.1-flatjar.jar",
+    ensure => present
+  }
+ 
+  file {"/etc/init.d/logstash":
+    source => "puppet:///modules/logstash/logstash",
+    ensure => present,
+    mode => 0755,
+    owner => root,
+    group => root
+  }
+ 
+  file {"/etc/logstash.conf":
+    source => "puppet:///modules/logstash/logstash.conf",
+    ensure => present,
+    replace => false
+  }
+ 
+  service {"logstash":
+    ensure => running,
+    subscribe => File["/etc/logstash.conf"],
+    require => [
+      File["/etc/init.d/logstash"],
+      File["/etc/logstash.conf"],
+      File["/opt/logstash-1.2.1-flatjar.jar"]
+    ]
+  }
+}
