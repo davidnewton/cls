81bc2d46118687dfdfb2fbb44cf274659cb53320
davidnewton <david.newton@esendex.com>
2013-09-25 14:01:49 +0100

    automated commit

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,8 @@
-node 'log-01' {
+node 'log-01.datacentre.esendex.com' {
   include puppet
   import 'es-01.pp'
 }
-node 'log-02' {
+node 'log-02.datacentre.esendex.com' {
   include puppet
   import 'collector.pp'
 }
