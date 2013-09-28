0bd620d6c99554b3177571de25bb982eb1a7c16a
davidnewton <david.newton@esendex.com>
2013-09-29 00:52:51 +0100

    automated commit

--- manifests/collector.pp
+++ manifests/collector.pp
@@ -4,6 +4,16 @@
    jarfile        => 'puppet:///modules/logstash/logstash-1.2.1-flatjar.jar',
    instances      => [ 'collector', 'shipper' ],
   }
+  file { '/etc/logstash/files/patterns':
+    ensure => "directory",
+    mode   => '0755',  
+  }
+  
+  file { '/etc/logstash/files/patterns/extra-patterns':
+    source => 'puppet:///modules/puppet/extra-patterns',
+    mode   => '0755',
+  }
+
   logstash::input::udp { 'logstash-udp':
    instances => [ 'collector' ],
    type => 'udp',
