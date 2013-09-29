726c300302191fa874b906162be7b61664e14d9d
logadmin <logadmin@log-02.datacentre.esendex.com>
2013-09-29 01:36:29 +0100

    automated commit

--- manifests/collector.pp
+++ manifests/collector.pp
@@ -23,7 +23,7 @@
   logstash::filter::grok { 'logstash-grok':
     instances    => [ 'collector' ],
     type         => 'udp',
-    match        => [ "message", "%{SYSLOG5424PRI:Priority} (?:%{TIMESTAMP_ISO8601:syslog5424_ts}|-) (?:%{HOSTNAME:syslog5424_host}|-) %{GREEDYDATA:syslog5424_msg}" ],
+    match        => [ 'message', '%{SYSLOG5424PRI:Priority} (?:%{TIMESTAMP_ISO8601:syslog5424_ts}|-) (?:%{HOSTNAME:syslog5424_host}|-) %{GREEDYDATA:syslog5424_msg}' ],
     add_tag      => [ 'syslog' ],
   }    
 
