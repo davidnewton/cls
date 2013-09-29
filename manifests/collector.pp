9d13ac60371233f49eed307ea4632880037da18a
logadmin <logadmin@log-02.datacentre.esendex.com>
2013-09-29 02:18:00 +0100

    automated commit

--- manifests/collector.pp
+++ manifests/collector.pp
@@ -13,22 +13,9 @@
     source => 'puppet:///modules/logstash/extra-patterns',
     mode   => '0755',
   }
-
-  logstash::input::udp { 'logstash-udp':
-   instances => [ 'collector' ],
-   type => 'udp',
-   port => '514',
+  
+  file { '/etc/logstash/collector/config/collector':
+    source => 'puppet:///modules/logstash/collector.config',
+    mode   => '0755',
   }
-
-  logstash::filter::grok { 'logstash-grok':
-    instances    => [ 'collector' ],
-    type         => 'udp',
-    { match => [ "@message", "%{SYSLOG5424PRI:Priority} (?:%{TIMESTAMP_ISO8601:syslog5424_ts}|-) (?:%{HOSTNAME:syslog5424_host}|-) %{GREEDYDATA:syslog5424_msg}" ]},
-    add_tag      => [ 'syslog' ],
-  }    
-
-  logstash::output::elasticsearch { 'logstash-es':
-   instances => [ 'collector' ],
-   type => 'udp',
-   host => 'log-01.datacentre.esendex.com',
-  }
+  
